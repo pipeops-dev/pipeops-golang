82ed84eca47b64cc59d8c5d82888986ebd079275
PipeOps Sales <sales@pipeops.io>
2024-06-07 13:53:57 +0100

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	r := gin.Default()
 	r.GET("", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello from pipeops",
+			"message": "Hello from PipeOps",
 		})
 	})
 	r.GET("/ping", func(c *gin.Context) {
